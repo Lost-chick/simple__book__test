97f313b9a7cdb2ecf333af3ae333aa89bf30e16f
Lost-chick <753402868@qq.com>
2020-02-22 11:36:00 +0800

    eject

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2020-02-22 11:21:14
+ * @LastEditTime: 2020-02-22 11:27:04
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \simple_book\src\App.js
+ */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     hello world
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2020-02-22 11:21:14
+ * @LastEditTime: 2020-02-22 11:31:46
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \simple_book\src\index.js
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
